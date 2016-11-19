e5b24344e0f1f8cf6f7911d4acf1c859129f78ad
sebastian <sebmuell@phys.ethz.ch>
2016-11-19 13:47:49 +0100

    split off the file handling and the plenoscope hit

--- bernlohr/iact.c
+++ bernlohr/iact.c
@@ -101,8 +101,7 @@ void extprm_ (
 /* CORSIKA function called from this module: */
 extern double heigh_ (double *thickness);
 
-/* =============================================================== */
-/* The ACP */
+//-------------------- PhotonBunch ---------------------------------------------
 struct PhotonBunch{
    float size;
    float x;
@@ -116,23 +115,145 @@ struct PhotonBunch{
    float mother_charge;
 };
 
-int SEED = -1;
-FILE *acp_photon_block;
-char acp_out_path[1024] = "";
+//-------------------- AcpIo --------------------------------------------------
+struct AcpIo {
+   char output_path [1024];
+   char runh_path [1024];
+   char evth_template_path [1024];
+   char photon_block_template_path [1024];
+   char readme_path [1024];
+   FILE *current_photons;
+};
+
+void AcpIo_init(struct AcpIo* sane, char* output_path) {
+   sane->current_photons = NULL;
+   strcpy(sane->output_path, output_path);
+
+   strcpy(sane->runh_path, sane->output_path);
+   strcat(sane->runh_path, ".runh");
+
+   strcpy(sane->evth_template_path, sane->output_path);
+   strcat(sane->evth_template_path, ".evth.");
+
+   strcpy(sane->photon_block_template_path, sane->output_path);
+   strcat(sane->photon_block_template_path, ".bunches.");
+
+   strcpy(sane->readme_path, sane->output_path);
+   strcat(sane->readme_path, ".README.md");
+}
+
+void AcpIo_write_readme(struct AcpIo* sane) {
+   char readme[] = 
+   "Atmospheric Cherenkov Plenoscope (ACP)\n"
+   "--------------------------------------\n"
+   "\n"
+   ".runh\n"
+   "-----\n"
+   "     float32 array [273 x 1], CORSIKA run header.\n"
+   "\n"
+   ".evth.XXX\n"
+   "---------\n"
+   "     float32 array [273 x 1], CORSIKA event header of event XXX.\n"
+   "\n"
+   ".bunches.XXX\n"
+   "------------\n"
+   "     float32 array [number_of_bunches x 10], Photon bunches of event XXX.\n"
+   "\n"
+   "Photon bunch\n"
+   "------------\n"
+   "     float32  size  bunch size (number of )\n"
+   "     float32  x     incident position on observation plane\n"
+   "     float32  y     incident position on observation plane\n"
+   "     float32  cx    incident angel relative to surface normal of observation plane\n"
+   "     float32  cy    incident angel relative to surface normal of observation plane\n"
+   "     float32  arrival time on observation plane, relative to primary's first interaction\n"
+   "     float32  emission altitude above sea level (not above observation plane)\n"
+   "     float32  wavelength in nano meter\n"
+   "     float32  mother particle mass in GeV\n"
+   "     float32  mother particle electric charge\n"
+   "\n"
+   "     Total bunch size is 10 x 4 bytes = 40 bytes\n"
+   "\n"
+   "To blame\n"
+   "--------\n"
+   "     Sebastian Achim Mueller, ETH Zurich 2016.\n";
+   FILE* out;
+   out = fopen(sane->readme_path, "w");
+   fputs(readme, out);
+   fclose(out);    
+}
+
+void AcpIo_evth_path(struct AcpIo* sane, int event_number, char* evth_path) {
+   char evt_num[1024];
+   sprintf(evt_num, "%d", event_number);
+   strcpy(evth_path, sane->evth_template_path);
+   strcat(evth_path, evt_num);
+}
+
+void AcpIo_photons_path(struct AcpIo* sane, int event_number, char* photons_path) {
+   char evt_num[1024];
+   sprintf(evt_num, "%d", event_number);
+   strcpy(photons_path, sane->photon_block_template_path);
+   strcat(photons_path, evt_num);
+}
+
+void AcpIo_write_runh(struct AcpIo* sane, cors_real_t runh[273]) {
+   AcpIo_write_readme(sane);
+   FILE* out;
+   out = fopen(sane->runh_path, "wb");
+   fwrite(&runh, sizeof(cors_real_t), 273, out);
+   fclose(out);  
+}
+
+void AcpIo_write_evth(struct AcpIo* sane, cors_real_t evth[273], int event_number) {
+   char evth_path[1024];
+   AcpIo_evth_path(sane, event_number, evth_path);
+   FILE* out;
+   out = fopen(evth_path, "wb");
+   fwrite(&evth, sizeof(cors_real_t), 273, out);
+   fclose(out);  
+}
+
+void AcpIo_open_photon_block(struct AcpIo* sane, int event_number) {
+   char photons_path[1024];
+   AcpIo_photons_path(sane, event_number, photons_path);
+   sane->current_photons = fopen(photons_path, "wb");  
+}
+
+void AcpIo_append_photon_bunch(struct AcpIo* sane, struct PhotonBunch* bunch) {
+   fwrite(bunch, sizeof((*bunch)), 1, sane->current_photons);
+}
+
+void AcpIo_close_photon_block(struct AcpIo* sane) {
+   fclose(sane->current_photons); 
+}
+
+//-------------------- Plenoscope ----------------------------------------------
 struct Plenoscope {
    double x;
    double y;
    double z;
    double radius;
 };
+
+int Plenoscope_is_hit_by_photon(struct Plenoscope* pl, struct PhotonBunch* bunch) {
+   double distance_to_plenoscope_center = sqrt(bunch->x*bunch->x + bunch->y*bunch->y);
+   return pl->radius >= distance_to_plenoscope_center;
+}
+
+//-------------------- init-- --------------------------------------------------
+int SEED = -1;
+char output_path[1024] = "";
+
 struct Plenoscope plenoscope;
+struct AcpIo acp_out;
 
 /**
  * Define the output file for photon bunches hitting the telescopes.
  * @param  name    Output file name.
 */
 void telfil_ (char *name) {
-   strcpy(acp_out_path, name);
+   strcpy(output_path, name);
    return;
 }
 
@@ -189,14 +310,8 @@ void telrnh_ (cors_real_t runh[273]) {
    plenoscope.z = 0.0;
    plenoscope.radius = 55.0e2;
 
-   FILE *acp_run_header;
-
-   char runh_path[1024];
-   strcpy(runh_path, acp_out_path);
-   strcat(runh_path, ".runh");
-   acp_run_header = fopen(runh_path, "wb");
-   fwrite(&runh, sizeof(cors_real_t), 273, acp_run_header);
-   fclose(acp_run_header);
+   AcpIo_init(&acp_out, output_path);
+   AcpIo_write_runh(&acp_out, runh);
 }
 
 
@@ -264,22 +379,10 @@ void telset_ (
  *  @return (none)
 */
 void televt_ (cors_real_t evth[273], cors_real_dbl_t prmpar[PRMPAR_SIZE]) {
-   FILE *acp_evt_header;
-   char evt_num[1024];
-   sprintf(evt_num, "%d", (int)evth[2-1]);
-   char evth_path[1024];
-   strcpy(evth_path, acp_out_path);
-   strcat(evth_path, ".evth.");
-   strcat(evth_path, evt_num);
-   acp_evt_header = fopen(evth_path, "wb");
-   fwrite(&evth, sizeof(cors_real_t), 273, acp_evt_header);
-   fclose(acp_evt_header);
 
-   char photon_block_path[1024];
-   strcpy(photon_block_path, acp_out_path);
-   strcat(photon_block_path, ".photons.");
-   strcat(photon_block_path, evt_num);   
-   acp_photon_block = fopen(photon_block_path, "wb");
+   int event_number = (int)evth[2-1];
+   AcpIo_write_evth(&acp_out, evth, event_number);
+   AcpIo_open_photon_block(&acp_out, event_number);
 }
 
 
@@ -334,9 +437,9 @@ int telout_ (
    bunch.mother_mass = *amass;
    bunch.mother_charge = *charge; 
 
-   double dist = sqrt(bunch.x*bunch.x + bunch.y*bunch.y);
-   if(dist <= plenoscope.radius)
-      fwrite(&bunch, sizeof(bunch), 1, acp_photon_block);
+   if(Plenoscope_is_hit_by_photon(&plenoscope, &bunch)) {
+      AcpIo_append_photon_bunch(&acp_out, &bunch);
+   }
    return 0;
 }
 
@@ -382,7 +485,7 @@ void tellng_ (
 */
 void telend_ (cors_real_t evte[273])
 {
-   fclose(acp_photon_block);
+   AcpIo_close_photon_block(&acp_out);
    return;
 }
 
