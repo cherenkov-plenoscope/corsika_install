888f2c532231d5a26fcf5af166f7d82942ef1f59
sebastian <sebmuell@phys.ethz.ch>
2016-11-20 21:06:49 +0100

    nice print

--- bernlohr/CherenkovInOut/PhotonBunch.h
+++ bernlohr/CherenkovInOut/PhotonBunch.h
@@ -15,4 +15,44 @@ struct PhotonBunch{
    float mother_charge;
 };
 
+void PhotonBunch_to_string(struct PhotonBunch* bunch, char* out) {
+   char size_str[1024];
+   sprintf(size_str, "%f", bunch->size);
+
+   char x_str[1024];
+   sprintf(x_str, "%f", bunch->x);
+
+   char y_str[1024];
+   sprintf(y_str, "%f", bunch->y);
+
+   char cx_str[1024];
+   sprintf(cx_str, "%f", bunch->cx);
+
+   char cy_str[1024];
+   sprintf(cy_str, "%f", bunch->cy);
+
+   char arrival_time_str[1024];
+   sprintf(arrival_time_str, "%f", bunch->arrival_time);
+
+   char emission_altitude_str[1024];
+   sprintf(emission_altitude_str, "%f", bunch->emission_altitude);
+
+   char wavelength_str[1024];
+   sprintf(wavelength_str, "%f", bunch->wavelength);
+
+   char mother_mass_str[1024];
+   sprintf(mother_mass_str, "%f", bunch->mother_mass);
+
+   char mother_charge_str[1024];
+   sprintf(mother_charge_str, "%f", bunch->mother_charge);
+
+   strcpy(out, "PhotonBunch(");
+   strcat(out, "size "); strcat(out, size_str); strcat(out, ", ");
+   strcat(out, "x "); strcat(out, x_str); strcat(out, "cm, ");
+   strcat(out, "y "); strcat(out, y_str); strcat(out, "cm, ");
+   strcat(out, "cx "); strcat(out, cx_str); strcat(out, ", ");
+   strcat(out, "cy "); strcat(out, cy_str); strcat(out, ", ");
+   strcat(out, ")");
+}
+
 #endif // __PhotonBunch_H_INCLUDED__ 
