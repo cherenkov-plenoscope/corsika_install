0db9401971c06c991ac95b55d3094be280910b15
sebastian <sebmuell@phys.ethz.ch>
2016-11-21 19:17:17 +0100

    a tiny first reader

--- custom_corsika/resources/CherenkovInOut/CherenkovInOut.py
+++ custom_corsika/resources/CherenkovInOut/CherenkovInOut.py
@@ -10,3 +10,21 @@ photon_dtype = np.dtype([
    ('mother_charge', np.int8),
    ('emission_altitude', np.int16),
 ])
+
+def read_compressed_photons(path):
+    return np.fromfile(path, dtype=photon_dtype)
+
+def decompress_photons(comp):
+    photons = np.zeros(shape=(comp.shape[0] ,8), dtype=np.float32)
+
+    int16max = 32767.0
+    int8max = 255.0
+    photons[:,0] = (comp['x']/int16max)*260.0e2
+    photons[:,1] = (comp['y']/int16max)*260.0e2
+    photons[:,2] = comp['cx']/int16max
+    photons[:,3] = comp['cy']/int16max
+    photons[:,4] = comp['arrival_time']
+    photons[:,5] = (comp['wavelength']/int8max)*1e3 + 2e2
+    photons[:,6] = comp['mother_charge']
+    photons[:,7] = (comp['emission_altitude']/int16max)*100*1e3*1e2
+    return photons
