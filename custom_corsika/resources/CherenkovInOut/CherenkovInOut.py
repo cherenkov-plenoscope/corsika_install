fe9164e3a7913f99f2e6a60caebb5e0a66f20e97
sebastian <sebmuell@phys.ethz.ch>
2016-11-21 18:52:43 +0100

    update struct ordering

--- custom_corsika/resources/CherenkovInOut/CherenkovInOut.py
+++ custom_corsika/resources/CherenkovInOut/CherenkovInOut.py
@@ -7,15 +7,6 @@ photon_dtype = np.dtype([
    ('cy', np.int16),
    ('arrival_time', np.float32),
    ('wavelength', np.int8),
-   ('emission_altitude', np.int16),
    ('mother_charge', np.int8),
+   ('emission_altitude', np.int16),
 ])
-
-int16_t x;
-int16_t y;
-int16_t cx;
-int16_t cy;
-float arrival_time;
-uint8_t wavelength;
-uint16_t emission_altitude;
-int8_t mother_charge;
